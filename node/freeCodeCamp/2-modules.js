b5761e096989d9f646579e7bd97a2e88428d6924
gabrielbigliardi <gabrielbigliardi@gmail.com>
2022-12-21 20:33:50 -0300

    estudos node

---
+++ node/freeCodeCamp/2-modules.js
@@ -0,0 +1,2 @@
+// Common js, every file is module (by default)
+// Modules - encapsulated code (only share minimum)
---
+++ node/freeCodeCamp/3-app.js
@@ -0,0 +1,7 @@
+
+const names = require('./3-names')
+const sayHi = require('./3-util')
+
+sayHi('maria')
+sayHi(names.john)
+sayHi(names.peter)
