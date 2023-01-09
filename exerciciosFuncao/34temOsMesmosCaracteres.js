dd6ee20516700a84708c27beaa95dec6251864c1
gabrielbigliardi <gabrielbigliardi@gmail.com>
2023-01-09 20:03:21 -0300

    formating same caracteres exercise

--- exerciciosFuncao/34temOsMesmosCaracteres.js
+++ exerciciosFuncao/34temOsMesmosCaracteres.js
@@ -1,12 +1,17 @@
 function contemMesmosCaracteres(str1, str2) {
     let estaContido = true
+
     if(str1.length !== str2.length) {
         return false
+
     } else {
+
         for(let i = 0; i < str1.length; i++) {
             let str1letter = str1[i].toLowerCase()
+
             for(let j = 0; j< str2.length; j++){
                 let str2letter = str2[j].toLowerCase()
+                
                 if(str1letter === str2letter) {
                     estaContido = true
                     break
@@ -22,4 +27,4 @@ function contemMesmosCaracteres(str1, str2) {
     }
 }
 
-console.log(contemMesmosCaracteres('abc', 'bcA'))
+console.log(contemMesmosCaracteres('ADCB', 'bcad'))
