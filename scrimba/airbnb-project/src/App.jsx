e6205454faa6244d64dd55a349809c49acf3741b
gabrielbigliardi <gabrielbigliardi@gmail.com>
2023-01-20 15:13:57 -0300

    starting airbnb dev

---
+++ scrimba/airbnb-project/src/App.jsx
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Navbar from './Navbar';
+import photoGrid from './imgs/photo-grid.png'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <photoGrid />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ scrimba/airbnb-project/src/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+import logo from './imgs/airbnb-logo.png'
+
+
+export default function Navbar() {
+    return (
+        <nav className='navbar'>
+            <img className='navbar--logo' src={logo} alt="Airbnb Logo"  />
+        </nav>
+    )
+}
