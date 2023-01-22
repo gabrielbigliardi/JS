b1c0d13407e9906f76ec5003f9849f66721de411
gabrielbigliardi <gabrielbigliardi@gmail.com>
2023-01-22 14:56:41 -0300

    adding badge

--- scrimba/airbnb-project/src/components/Card.jsx
+++ scrimba/airbnb-project/src/components/Card.jsx
@@ -6,10 +6,20 @@ import star from '../imgs/star.png'
 
 export default function Card(props) {
     // console.log(props)
+
+    let badgeText
+    if(props.openSpots === 0) {
+        badgeText = 'SOLD OUT'
+    } else if(props.location === 'Online') {
+        badgeText = 'ONLINE'
+    }
+
     return (
         <div className='card'>
+            {badgeText && <div className='card--badge'>{badgeText}</div>}
             <img className='card--img' src={ require(`../imgs/${props.img}`) } alt="Katie Zaferes" />
-            <span className='card--openspots'>SOLD OUT</span>
+            {/* {!props.openSpots && <span className='card--badge'>SOLD OUT</span>} */}
+            {/* <span className='card--openspots'>{props.openSpots > 0 ? 'ONLINE' : 'SOLD OUT'}</span> */}
             <div className='card--stats'>
                 <img className='card--star' src={star} alt="Star"  />
                 <span>{props.rating}</span>
