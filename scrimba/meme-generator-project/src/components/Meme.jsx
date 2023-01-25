11f3e5aae7a644a6fc015712339bb65e579ee18b
gabrielbigliardi <gabrielbigliardi@gmail.com>
2023-01-25 19:38:35 -0300

    restarting crud project

--- scrimba/meme-generator-project/src/components/Meme.jsx
+++ scrimba/meme-generator-project/src/components/Meme.jsx
@@ -1,13 +1,35 @@
+// import { random } from "lodash"
 import React from "react"
+import memesData from "../memesData"
 
 export default function Meme() {
+
+    // const [ memeImg, setMemeImg ] = React.useState()
+    const [ meme, setMeme ] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "https://i.imgflip.com/4xgqu.jpg"
+    })
+
+    const [ allMemes, setAllMemes ] = React.useState(memesData)
+
+    function getMeme() {
+        const memesArray = allMemes.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        // const { url } = memesArray[randomNumber]
+        const url = memesArray[randomNumber].url
+        console.log(url)
+        setMeme( prevMeme => ({ ...prevMeme, randomImage: url }))
+    }
+
     return (
-        <section className="meme">
+        <div className="meme">
             <div className="meme--inputs">
                 <input type="text" placeholder="Upper text" className="meme--up-input" />
                 <input type="text" placeholder="Down text" className="meme--down-input" />
             </div>
-            <button className="meme--button">Get a new meme image 🖼</button>
-        </section>
+            <button className="meme--button" onClick={getMeme} >Get a new meme image 🖼</button>
+            <img className="meme--img" src={meme.randomImage} alt="Meme" />
+        </div>
     )
 }
