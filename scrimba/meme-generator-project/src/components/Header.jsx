4d651061843b969dc9eb4302857d0791d8e9c9a0
gabrielbigliardi <gabrielbigliardi@gmail.com>
2023-01-25 19:37:30 -0300

    dev meme gen

--- scrimba/meme-generator-project/src/components/Header.jsx
+++ scrimba/meme-generator-project/src/components/Header.jsx
@@ -3,10 +3,12 @@ import './style.css'
 
 export default function Header() {
   return (
-    <nav className="meme--nav">
-      <img src={require(`../imgs/Troll Face.png`)} className="troll-face" />
-      <h1 className="meme--title">Meme Generator</h1>
-      <h3 className="meme--description">React Course - Project 3</h3>
+    <nav className="nav">
+      <div className="nav--troll--title">
+        <img src={require(`../imgs/Troll Face.png`)} className="nav--troll-face" alt='Troll' />
+        <h1 className="nav--title">Meme Generator</h1>
+      </div>
+      <h3 className="nav--description">React Course - Project 3</h3>
     </nav>
   )
 }
