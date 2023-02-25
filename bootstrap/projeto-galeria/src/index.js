cdb8b1ae1c5ebf41654219a6d3bd892928212089
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2023-02-25 19:57:06 -0300

    corrigindo projeto galeria/adding babel

---
+++ bootstrap/projeto-galeria/src/index.js
@@ -0,0 +1,10 @@
+// Arquivo SASS
+import './scss/index.scss'
+
+console.log('hello from webpack dev server')
+
+const hi = name => {
+    console.log(`Hi, ${name}`)
+}
+
+hi('Jumba')
--- bootstrap/projeto-galeria/webpack.config.js
+++ bootstrap/projeto-galeria/webpack.config.js
@@ -1,46 +1,67 @@
 const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
+const HtmlWebpack = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+// const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: modoDev ? 'development' : 'production',
     entry: './src/index.js',
     devServer: {
-        contentBase: './build',
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
         port: 9000,
     },
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true,
-                sourceMap: true
-            }),
-            new OptimizeCSSAssetsPlugin({})
-        ]
-    },
+    // devServer: {
+    //     contentBase: './dist',
+    //     port: 9000,
+    // },
+    // optimization: {
+    //     minimizer: [
+    //         new UglifyJsPlugin({
+    //             cache: true,
+    //             parallel: true,
+    //             sourceMap: true
+    //         }),
+    //         new OptimizeCSSAssetsPlugin({})
+    //     ]
+    // },
     output: {
-        filename: 'app.js',
-        path: __dirname + '/build'
+        filename: 'bundle.js',
+        path: path.resolve(__dirname + '/dist')
     },
     plugins: [
+        // new MiniCssExtractPlugin({ filename: 'estilo.css' }),
         new MiniCssExtractPlugin({ filename: 'estilo.css' }),
-        new CopyWebpackPlugin([
-            { context: 'src/', from: '**/*.html' },
-            { context: 'src/', from: 'imgs/**/*' }
-        ])
+        new HtmlWebpackPlugin({
+            title: 'Development'
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: './src/*.html', to: '/dist' },
+                { from: './src/imgs', to: 'imgs' }
+            ]
+        })
     ],
+
+    // [
+    //     { from: 'src/', to: '**/*.html' },
+    //     { from: 'src/', to: 'imgs/**/*' }
+    // ])
     module: {
         rules: [{
-            test: /\.s?[ac]ss$/,
+            // test: /\.s?[ac]ss$/,
+            test: /\.s?[ac]ss$/i,
             use: [
-                MiniCssExtractPlugin.loader,
-                // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                MiniCssExtractPlugin.loader, // Adiciona CSS a DOM injetando a tag <style>
                 'css-loader', // interpreta @import, url()...
-                'sass-loader',
+                'sass-loader'
             ]
         }, {
             test: /\.(png|svg|jpg|gif)$/,
@@ -48,6 +69,17 @@ module.exports = {
         }, {
             test: /.(ttf|otf|eot|svg|woff(2)?)$/,
             use: ['file-loader']
-        }]
+        }, {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
     }
 }
+
