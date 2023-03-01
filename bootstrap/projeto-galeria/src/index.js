2a57e0349066266b8673a65d4fc3f84102207f98
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2023-02-28 21:05:59 -0300

    sem funcionalidade

--- bootstrap/projeto-galeria/src/index.js
+++ bootstrap/projeto-galeria/src/index.js
@@ -1,6 +1,14 @@
 // Arquivo SASS
 import './scss/index.scss'
 
+//Dependencias
+import 'jquery'
+import 'bootstrap'
+
+//Meus arquivos JS
+import './js/core/includes'
+
+
 console.log('hello from webpack dev server')
 
 const hi = name => {
