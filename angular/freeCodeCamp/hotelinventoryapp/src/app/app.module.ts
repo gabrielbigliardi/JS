8b0c32c513133011631d523c97037b5b3bba9ce7
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2023-03-03 16:45:02 -0300

    directives / bootstrap

--- angular/freeCodeCamp/hotelinventoryapp/src/app/app.module.ts
+++ angular/freeCodeCamp/hotelinventoryapp/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoomsComponent } from './rooms/rooms.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { RoomsComponent } from './rooms/rooms.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/freeCodeCamp/hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ angular/freeCodeCamp/hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { Room } from './rooms';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
-export class RoomsComponent {
+export class RoomsComponent implements OnInit {
 
   hotelName = 'Jumba Hotel'
 
-  numberOfRooms = 10
+  // numberOfRooms = 10
 
   hiddenRooms = false
 
@@ -18,15 +18,48 @@ export class RoomsComponent {
     this.hiddenRooms = !this.hiddenRooms
   }
 
-  
-  rooms : Room = {
-    totalRooms: 20,
-    availableRooms: 0,
+
+  rooms: Room = {
+    totalRooms: 15,
+    availableRooms: 30,
     bookedRooms: 5,
   }
 
+
+  roomList: RoomList[] = []
+
   // constructor() {}
 
-  // ngOnInit(): void { }
+  ngOnInit(): void { 
+    this.roomList = [
+      {
+        roomNumber: 1,
+        roomType: 'Suite',
+        amenities: 'air conditioner, wi-fi, tv',
+        price: 250,
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 4.5,
+      }, {
+        roomNumber: 3,
+        roomType: 'Deluxe',
+        amenities: 'air conditioner, wi-fi, tv, kitchen',
+        price: 350,
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 4.732,
+  
+      }, {
+        roomNumber: 2,
+        roomType: 'Private Suite',
+        amenities: 'air conditioner, wi-fi, tv, kitchen',
+        price: 500,
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 4.8,
+  
+      }
+    ]
+  }
 
 }
