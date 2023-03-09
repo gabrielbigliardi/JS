ce84ead2cacf2d9489ee3256c3d62565fae4358d
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2023-03-09 17:02:56 -0300

    router

--- angular/horaDeCodar/curso-angular/src/app/app-routing.module.ts
+++ angular/horaDeCodar/curso-angular/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { PipesComponent } from './components/pipes/pipes.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
+import { InterpolationComponent } from './components/interpolation/interpolation.component';
+
+const routes: Routes = [
+  {path: '', component: InterpolationComponent },
+  {path: 'list', component: ListRenderComponent },
+  {path: 'pipes', component: PipesComponent },
+  {path: 'twowaydatabinding', component: TwoWayBindingComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)], //dizendo q as rotas começam no root da aplicação
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
