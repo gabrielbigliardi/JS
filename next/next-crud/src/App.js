6f02a6b5cae2b6f60dbc8017350bc267ae2bee0f
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2023-04-03 18:36:57 -0300

    starting nextjs, tailwind, firestone

---
+++ next/next-crud/src/App.js
@@ -0,0 +1,12 @@
+import './styles/App.css';
+import 'tailwindcss/tailwind.css'
+
+function App() {
+  return (
+    <div className={'flex h-screen justify-center items-center '}>
+      <span className='text-4xl'>Texto</span>
+    </div>
+  );
+}
+
+export default App;
