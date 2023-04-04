b35e0804eaadcefe5eae25ef6c80342a32b9042f
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2023-04-04 02:11:01 -0300

    tabela criada

--- next/next-crud/src/pages/index.tsx
+++ next/next-crud/src/pages/index.tsx
@@ -2,13 +2,42 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Layout from '@/components/Layout'
+import Tabela from '@/components/Tabela'
+import Cliente from '@/core/Cliente'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Jumba', 22, '1'),
+    new Cliente('Aurora', 8, '2'),
+    new Cliente('Bela', 5, '3'),
+    new Cliente('Jimi', 8, '4'),
+  ]
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome)
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(`Excluindo ${cliente.nome}`)
+  }
+
   return (
-    <div >
-      <span>Hello World</span>
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}>
+      <Layout titulo='Cadastro Simples' >
+        <Tabela clientes={clientes} 
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        />
+      </Layout>
+      <h1></h1>
     </div>
   )
 }
