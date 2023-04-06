79323c4f73f7d9739e315096eb4a6b7f535bd238
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2023-04-06 18:14:56 -0300

    api category

--- knowledge/versao-inicial/backend/config/routes.js
+++ knowledge/versao-inicial/backend/config/routes.js
@@ -7,4 +7,15 @@ module.exports = app => {
         .put(app.api.user.save)
         .get(app.api.user.getById)
 
+    app.route('/categories')
+        .get(app.api.category.get)
+        .post(app.api.category.save)
+
+    app.route('/categories/tree')
+        .get(app.api.category.getTree)
+
+    app.route('/categories/:id')
+        .get(app.api.category.getById)
+        .put(app.api.category.save)
+        .delete(app.api.category.remove)
 }
