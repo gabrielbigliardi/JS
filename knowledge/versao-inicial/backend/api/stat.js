7d370f10079a9e86618d3ed09893b0d087b72621
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2023-04-11 15:56:00 -0300

    defaultStat

--- knowledge/versao-inicial/backend/api/stat.js
+++ knowledge/versao-inicial/backend/api/stat.js
@@ -8,7 +8,14 @@ module.exports = app => {
 
     const get = (req, res) => {
         Stat.findOne({}, {}, { sort: { 'createdAt': -1 } })
-            .then(stat => res.json(stat))
+            .then(stat => {
+                const defaultStat = {
+                    users: 0,
+                    categories: 0,
+                    articles: 0,
+                }
+                res.json(stat || defaultStat)
+            })
     }
 
     return { Stat, get }
