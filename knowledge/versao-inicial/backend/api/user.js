cba32fbe132cb1636bcba4c08aab439e02d4b958
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2023-04-14 00:39:14 -0300

    user backend api

--- knowledge/versao-inicial/backend/api/user.js
+++ knowledge/versao-inicial/backend/api/user.js
@@ -16,6 +16,9 @@ module.exports = app => {
         // console.log(user)
         if(req.params.id) user.id = req.params.id
 
+        if(!req.originalUrl.startsWith('/users')) user.admin = false
+        if(!req.user || !req.user.admin) user.admin = false
+
         try {
             existsOrError(user.name, 'Nome não informado')
             existsOrError(user.email, 'Email não informado')
