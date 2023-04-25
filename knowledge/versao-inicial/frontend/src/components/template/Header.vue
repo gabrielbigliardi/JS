cc347108604e1bfa4a4819c93bc1e789ee9864e4
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2023-04-25 01:55:04 -0300

    knowledge start front

---
+++ knowledge/versao-inicial/frontend/src/components/template/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="header"></header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+    .header {
+        grid-area: header;
+        /* background-color: linear-gradient(to right, #1e469a, #49a7c1); */
+        background-color: #1e469a;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
