bb5e4f84002db1e0ee0cfee011955b6b19ae4cdb
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2023-06-06 18:25:49 -0300

    starting nesting routes / Layout and Outlet

--- scrimba/learn-react-router-6-main/02 - Nested Routes/02 - Fixing the Navbar with a Layout Route/index.jsx
+++ scrimba/learn-react-router-6-main/02 - Nested Routes/02 - Fixing the Navbar with a Layout Route/index.jsx
@@ -5,19 +5,13 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
 import VanDetail from "./pages/VanDetail"
+import Header from './components/Header'
 
 import "./server"
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- scrimba/learn-react-router-6-main/02 - Nested Routes/03 - Fixing the Navbar with a Layout Route part 2/index.jsx
+++ scrimba/learn-react-router-6-main/02 - Nested Routes/03 - Fixing the Navbar with a Layout Route part 2/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
 import VanDetail from "./pages/VanDetail"
+import Layout from './components/Layout';
 
 import "./server"
 
@@ -12,10 +14,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
